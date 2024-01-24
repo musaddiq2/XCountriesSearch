3007afa5d01ddad3ddce6ded0ca0df70f995e480
musaddiq2 <uzumakibaruto093@gmail.com>
2024-01-24 16:43:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const CountryCard = ({ name, flags }) => (
+  <div className="card">
+    <img src={flags.png} alt={`Flag of ${name}`} />
+    <h2>{name.common}</h2>
+    <p>{name.official}</p>
+  </div>
+);
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  React.useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        setCountries(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, []);
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search for a country..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
+      <div id="root">
+        {filteredCountries.map((country) => (
+          <CountryCard key={country.cca3} {...country} />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
